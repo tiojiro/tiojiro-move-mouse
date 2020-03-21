004a53c623f37d1072a358487b6ac463e1bc4e7a
Renato Tiojiro Ugajin Zukeran <tiojiro@TiojiroMBP.local>
2020-03-21 13:00:02 +0000

    ajuste macos

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def start_move(lbl_run):
         if stop:
             break
         x = window.winfo_x() + 70
-        y = window.winfo_y() + 140
+        y = window.winfo_y() + 10
         autopy.mouse.smooth_move(x,y)
         autopy.mouse.click()
         if stop:
@@ -40,7 +40,7 @@ def start_move(lbl_run):
         if stop:
             break
         x = window.winfo_x() + 190
-        y = window.winfo_y() + 140
+        y = window.winfo_y() + 10
         autopy.mouse.smooth_move(x,y)
         autopy.mouse.click()
 
@@ -89,14 +89,16 @@ def config_components(win):
     #label Frame para os comandos
     labelframe = tk.LabelFrame(win, text=const.LF_COMMANDS)
     labelframe.pack(fill=tk.BOTH, expand=tk.YES)
+    labelframe.grid_rowconfigure(0, weight=0)
+    labelframe.grid_columnconfigure(0, weight=1)
 
     #botao para iniciar o mouse
-    btn_start = tk.Button(labelframe, text=const.BTN_TEXT_START, height=1, width=30, padx=0, pady=0, command= lambda:btn_start_click(lbl_run))
-    btn_start.grid(column=1, row=0, padx=12, pady=5, sticky=tk.W)
+    btn_start = tk.Button(labelframe, text=const.BTN_TEXT_START, height=1, command= lambda:btn_start_click(lbl_run))
+    btn_start.grid(column=0, row=0, padx=12, pady=5, sticky=tk.NSEW)
     
     #botao para parar o mouse
-    btn_start = tk.Button(labelframe, text=const.BTN_TEXT_STOP, height=1, width=30, padx=0, pady=0, command= lambda:btn_stop_click(lbl_run))
-    btn_start.grid(column=1, row=1, padx=12, pady=5, sticky=tk.W)
+    btn_start = tk.Button(labelframe, text=const.BTN_TEXT_STOP, height=1, command= lambda:btn_stop_click(lbl_run))
+    btn_start.grid(column=0, row=1, padx=12, pady=5, sticky=tk.NSEW)
 
     #label para indicar a execucao do mouse
     lbl_run = tk.Label(win, bd=1, relief=tk.SUNKEN, anchor=tk.W)
@@ -111,7 +113,7 @@ window.resizable(0, 0)
 window.config(menu=config_menu(window))
 #janela da aplicacao sempre na frente em relacao aos outros programas
 window.attributes('-topmost', True)
-window.wm_iconbitmap(default=favicon.favicon)
+window.wm_iconbitmap(favicon.favicon)
 #sobreescrever o atl+F4 e o X do windows e sempre passar pelo metodo quit_app
 #com isso garante que sera fechado a thread caso esteja ativa
 window.protocol("WM_DELETE_WINDOW", quit_app)
